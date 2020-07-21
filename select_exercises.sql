856ef2cd864665233651fa3563136da73ec7a2bb
Bethany Thompson <thompson.bethany.01@gmail.com>
2020-07-21 14:42:58 -0500

    Adds Previous Exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,36 +1,36 @@
---Use the albums_db database.
+-- Use the albums_db database.
 USE albums_db;
 
---Explore the structure of the albums table.
+-- Explore the structure of the albums table.
 DESCRIBE albums;
 
---Write queries to find the following information.
---The name of all albums by Pink Floyd
+-- Write queries to find the following information.
+-- The name of all albums by Pink Floyd
 SELECT artist, name
 FROM albums
 WHERE artist = 'Pink Floyd';
 
---The year Sgt. Pepper's Lonely Hearts Club Band was released
+-- The year Sgt. Pepper's Lonely Hearts Club Band was released
 SELECT artist, name, release_date
 FROM albums
 WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 
---The genre for the album Nevermind
+-- The genre for the album Nevermind
 SELECT artist, name, genre
 FROM albums
 WHERE name = 'Nevermind';
 
---Which albums were released in the 1990s
+-- Which albums were released in the 1990s
 SELECT artist, name, release_date
 FROM albums
 WHERE release_date BETWEEN 1990 and 1999;
 
---Which albums had less than 20 million certified sales
+-- Which albums had less than 20 million certified sales
 SELECT artist, name, sales
 FROM albums
 WHERE sales < 20;
 
---All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+-- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
 SELECT artist, name, genre
 FROM albums
 WHERE genre = 'Rock';
@@ -39,7 +39,7 @@ SELECT artist, name, genre
 FROM albums
 WHERE genre = 'rock' OR genre = 'Hard Rock' OR genre = 'Progressive Rock' OR genre = 'Alternative Rock';
 
---'Rock' != 'Hard Rock', they are not equivalent
+-- 'Rock' != 'Hard Rock', they are not equivalent
 
 SELECT *
 FROM albums
